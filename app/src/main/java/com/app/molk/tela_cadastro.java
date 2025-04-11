28d2b0d5bfcfac05b933f971568b599f0e93aa9d
laurabonilha <bonilha.laura@gmail.com>
2025-04-10 21:53:09 -0300

    Adicionada tela de cadastro de usuário

---
+++ app/src/main/java/com/app/molk/tela_cadastro.java
@@ -0,0 +1,14 @@
+package com.app.molk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.app.molk.R;
+
+public class tela_cadastro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
--- app/src/main/java/com/app/molk/tela_principal.java
+++ app/src/main/java/com/app/molk/tela_principal.java
@@ -1,7 +1,6 @@
 package com.app.molk;
 
 import android.os.Bundle;
-
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
@@ -14,11 +13,22 @@ public class tela_principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
 
+        // Botão para ir para a tela de login
         Button btnLogin = findViewById(R.id.btn_login);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(tela_principal.this, tela_login.class);
+                startActivity(intent);
+            }
+        });
+
+        // Botão para ir para a tela de cadastro
+        Button btnCadastrar = findViewById(R.id.btn_cadastrar);
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(tela_principal.this, tela_cadastro.class);
                 startActivity(intent);
             }
         });
